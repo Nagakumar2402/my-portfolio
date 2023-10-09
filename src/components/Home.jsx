afa91cfce53964a741f1c59a388e2041d2c9e0b2
Nagakumar2402 <nagakumar2402@gmail.com>
2023-10-09 20:23:04 +0530

    next skil added

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -1,28 +1,34 @@
-import React from 'react';
-import { HiArrowNarrowRight } from 'react-icons/hi';
+import React from "react";
+import { HiArrowNarrowRight } from "react-icons/hi";
 
 const Home = () => {
   return (
-    <div name='home' className='w-full h-screen '>
+    <div name="home" className="w-full h-screen ">
       {/* Container */}
-      <div className='max-w-[1000px] mx-auto px-8  flex flex-col justify-center h-full'>
-        <p className=' text-pink-600 capitalize text-lg font-sans'>Hi, my name is</p>
-        <h1 className='text-4xl sm:text-7xl font-bold text-[#ccd6f6]'>
+      <div className="max-w-[1000px] mx-auto px-8  flex flex-col justify-center h-full">
+        <p className="font-sans text-lg text-pink-600 capitalize ">
+          Hi, my name is
+        </p>
+        <h1 className="text-4xl sm:text-7xl font-bold text-[#ccd6f6]  ">
           Naga Kumar
         </h1>
-        <h2 className='text-4xl sm:text-7xl font-bold text-[#bac0d3]/80 my-4'>
-          I'm a <span className='underline decoration-pink-600/50'>Full Stack Developer</span>.
+        <h2 className="text-4xl sm:text-7xl font-bold text-[#bac0d3]/80 my-4">
+          I'm a{" "}
+          <span className="underline decoration-pink-600/50">
+            Full Stack Developer
+          </span>
+          .
         </h2>
-        <p className='text-[#8892b0] py-4 max-w-[700px] leading-6'>
+        <p className="text-[#8892b0] py-4 max-w-[700px] leading-6">
           I’m a full-stack developer specializing in building (and occasionally
           designing) exceptional digital experiences. Currently, I’m focused on
           building responsive full-stack web applications.
         </p>
         <div>
-          <button className='text-white group border-2 px-6 py-3 my-2 flex items-center hover:bg-pink-600 hover:border-pink-600'>
+          <button className="flex items-center px-6 py-3 my-2 text-white border-2 group hover:bg-pink-600 hover:border-pink-600">
             View Work
-            <span className='group-hover:rotate-90 duration-300'>
-              <HiArrowNarrowRight className='ml-3 ' />
+            <span className="duration-300 group-hover:rotate-90">
+              <HiArrowNarrowRight className="ml-3 " />
             </span>
           </button>
         </div>
