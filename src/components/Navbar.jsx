57ba7d98210851b53d04f3e2a3166e38adbd80be
nagakumar2402 <nagakumar2402@gmail.com>
2023-10-09 17:10:46 +0530

    1st commit

---
+++ src/components/Navbar.jsx
@@ -0,0 +1,130 @@
+import React, { useState } from 'react';
+import {
+  FaBars,
+  FaTimes,
+  FaGithub,
+  FaLinkedin,
+  FaFacebook,
+  FaLinkedinIn,
+} from 'react-icons/fa';
+import { HiOutlineMail } from 'react-icons/hi';
+import { BsFillPersonLinesFill } from 'react-icons/bs';
+import Logo from '../assets/logo.png';
+import { Link  } from 'react-scroll';
+
+const menu = [
+  {
+    id: 1,
+    title: 'Home',
+    link:"home"
+  },{
+    id: 2,
+    title: 'About',
+    link: 'about'
+  },{
+    id: 3,
+    title: 'Skills',
+    link: 'skills'
+  },{
+    id: 4,
+    title: 'Work',
+    link: 'work'
+  },{
+    id: 5,
+    title: 'Contact',
+    link: 'contact'
+  }
+]
+
+const Navbar = () => {
+  const [nav, setNav] = useState(false);
+  const handleClick = () => setNav(!nav);
+
+  return (
+    <div className='fixed w-full h-[80px] flex justify-between items-center px-8 bg-[#A78BFA]'>
+      <div>
+        {/* <img src={Logo} alt='Logo Image' style={{ width: '200px' }} /> */}
+        <h1>Naga Kumar</h1>
+      </div>
+
+      {/* menu */}
+      <ul className='items-stretch hidden space-x-3 lg:flex'>
+
+      {
+          menu.map(item => (
+            <li key={item.id} className="flex hover:text-white duration-200 transition-all translate ">
+          <Link to={item.link} smooth={true} duration={500} className='font-bold' >
+            {item.title}
+          </Link>
+        </li>
+          ))
+        }
+      
+      </ul>
+
+      {/* Hamburger */}
+      <div onClick={handleClick} className='md:hidden z-10'>
+        {!nav ? <FaBars /> : <FaTimes />}
+      </div>
+
+      {/* Mobile menu */}
+      <ul
+        className={
+          !nav
+            ? 'hidden'
+            : 'absolute top-0 left-0 w-full h-screen bg-[#0a192f] flex flex-col justify-center items-center'
+        }
+      >
+        {
+          menu.map(item => (
+            <li key={item.id} className='py-6 text-4xl text-white'>
+          <Link to={item.link} onClick={handleClick} smooth={true} duration={500} className='font-bold' >
+            {item.title}
+          </Link>
+        </li>
+          ))
+        }
+      </ul>
+
+      {/* Social icons */}
+      <div className='hidden lg:flex fixed flex-col top-[35%] left-0 '>
+        <ul>
+          <li className='w-[160px] h-[60px] flex justify-between items-center ml-[-100px] hover:ml-[-10px] duration-300 bg-blue-600 '>
+            <a
+              className='flex justify-between items-center w-full text-gray-300'
+              href='/'
+            >
+              Linkedin <FaLinkedin size={30} />
+            </a>
+          </li>
+          <li className='w-[160px] h-[60px] flex justify-between items-center ml-[-100px] hover:ml-[-10px] duration-300 bg-[#333333]'>
+            <a
+              className='flex justify-between items-center w-full text-gray-300'
+              href='/'
+            >
+              Github <FaGithub size={30} />
+            </a>
+          </li>
+          <li className='w-[160px] h-[60px] flex justify-between items-center ml-[-100px] hover:ml-[-10px] duration-300 bg-[#6fc2b0]'>
+            <a
+              className='flex justify-between items-center w-full text-gray-300'
+              href='/'
+            >
+              Email <HiOutlineMail size={30} />
+            </a>
+          </li>
+          <li className='w-[160px] h-[60px] flex justify-between items-center ml-[-100px] hover:ml-[-10px] duration-300 bg-[#565f69]'>
+            <a
+              className='flex justify-between items-center w-full text-gray-300'
+              href='/'
+            >
+              Resume <BsFillPersonLinesFill size={30} />
+            </a>
+          </li>
+        </ul>
+      </div>
+    </div>
+  );
+};
+
+export default Navbar;
